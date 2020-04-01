9129d4ab218dca7bbbcf99427ed403ad30e97250
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2020-04-01 22:54:21 +0900

    first-commit

--- front/src/components/CalendarBoard/container.jsx
+++
@@ -1,6 +0,0 @@
-import { connect } from "react-redux";
-import CalendarBoard from "./presentation";
-
-const mapStateToProps = state => ({ calendar: state.calendar });
-
-export default connect(mapStateToProps)(CalendarBoard);
